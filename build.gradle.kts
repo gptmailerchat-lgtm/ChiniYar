59f6ab8c34acdb1f3b22ac1da923655f9db13594
gptmailerchat-lgtm <gptmailerchat@gmail.com>
2026-08-22 22:32:27 -0700

    Add root Gradle build configuration

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+// ChiniYar build configuration.
+// Direct plugin artifacts are used instead of plugin marker resolution.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:9.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.21")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
